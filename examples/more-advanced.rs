0f95f3f9b27fcf338496f3ca27e04603245b76f8
D.S. Ljungmark <spider@skuggor.se>
2020-08-20 15:05:56 +0200

    examples: Make the intermediate example show how to load keys
    This makes the advanced example load a private key from the environment, and
    treats all other data as ephemeral during run-time.

--- examples/more-advanced.rs
+++ examples/more-advanced.rs
@@ -2,6 +2,12 @@ use caramel_client;
 
 const CA_SERVER: &str = "ca.sommar.modio.se";
 
+// This example attempts to show how to use the Caramel Client library where we do not store data
+// as files on disk, but in some kind of external storage.
+// In this explicit case, we use an environment variable for the Private Key, and always
+// re-generate the CSR (Certificate Sign Request).
+//
+//
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // use example `hostname` or `cat /etc/machine-id`  to identify a single machine.
     let client_id = "test-certificate-please-ignore";
@@ -19,10 +25,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ///
     /// Note that it does NOT implement any file IO, but it does implement Network IO.
     let mut caramel_request = caramel_client::CaramelRequest::new(CA_SERVER, client_id);
-    caramel_request.get_cacert().expect("Error fetching CA Certificate from server");
-    caramel_request.new_key().expect("Error generating Private Key Pair");
+    caramel_request
+        .get_cacert()
+        .expect("Error fetching CA Cert from server");
+
+    // We try to load the private key from somewhere else.
+    // In this case, an environment variable.
+    // Do note that it's not recommended to store sensitive data as private keys in environment
+    // variables, even if Kubernetes and Docker recommends it.
+    // However, it is a convenient way to show the API.
+    let env_privkey = std::env::var_os("PRIVATE_KEY");
+    if let Some(val) = env_privkey {
+        let key_data = val
+            .into_string()
+            .expect("Environment variable could not be parsed to string.");
+        caramel_request
+            .load_key(&key_data)
+            .expect("Error loading Private Key Pair from external source.");
+    } else {
+        caramel_request
+            .new_key()
+            .expect("Error generating Private Key Pair");
+    }
+    println!("This is your private key. Save it somewhere safe.");
     println!("{}", caramel_request.key_data);
-    caramel_request.new_csr()
+    /// Due to caramel design choices, it is safe to always re-generate the CSR data.
+    /// As long as the CA Certificate, Client ID and Private Key are the same, the Request should
+    /// always be bit-by-bit identical.
+    caramel_request
+        .new_csr()
         .expect("Error generating Certificate Sign Request");
     println!("{}", caramel_request.csr_data);
     caramel_request
