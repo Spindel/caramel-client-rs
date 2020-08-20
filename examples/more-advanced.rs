f65903320eb9d0436beea14a8f3c954ef98f42f6
D.S. Ljungmark <spider@skuggor.se>
2020-08-20 15:05:33 +0200

    examples: Expand variable names to be clearer
    It is better to be verbose in example code.

--- examples/more-advanced.rs
+++ examples/more-advanced.rs
@@ -10,24 +10,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ///
     ///
     /// This API exposes the following functions that are useful:
-    ///     get_cacert              - Download a CA certificate from server
-    ///     load_cacert             - Load a CA certificate from a buffer
+    ///     get_cacert              - Download a CA Certificate from server
+    ///     load_cacert             - Load a CA Certificate from a buffer
     ///     new_key / load_key      - Makes a Private Key available
     ///     new_csr / load_csr      - Makes a CSR available
     ///     post_csr                - Post the CSR to the Certificate Authority
     ///     get_crt                 - Fetch a certificate from the Certificate Authority
     ///
     /// Note that it does NOT implement any file IO, but it does implement Network IO.
-    let mut CR = caramel_client::CaramelRequest::new(CA_SERVER, client_id);
-    CR.get_cacert().expect("Error fetching CA Cert from server");
-    CR.new_key().expect("Error generating Private Key Pair");
-    println!("{}", CR.key_data);
-    CR.new_csr()
+    let mut caramel_request = caramel_client::CaramelRequest::new(CA_SERVER, client_id);
+    caramel_request.get_cacert().expect("Error fetching CA Certificate from server");
+    caramel_request.new_key().expect("Error generating Private Key Pair");
+    println!("{}", caramel_request.key_data);
+    caramel_request.new_csr()
         .expect("Error generating Certificate Sign Request");
-    println!("{}", CR.csr_data);
-    CR.post_csr()
+    println!("{}", caramel_request.csr_data);
+    caramel_request
+        .post_csr()
         .expect("Error posting Certificate Sign Request");
-    CR.get_crt()
+    caramel_request
+        .get_crt()
         .expect("Error downloading certificate from server");
-    println!("{}", CR.crt_data);
+    println!("This is our signed certificate. Store it or use it.");
+    println!("{}", caramel_request.crt_data);
 }
--- examples/simplest.rs
+++ examples/simplest.rs
@@ -13,13 +13,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ///
     /// Note that the FileRequest will invariably _delete_ local data (key, csr, crt)  and
     /// re-create them if it gets errors from the server.
-    let mut CR = caramel_client::FileRequest::new(CA_SERVER, client_id);
+    let mut caramel_request = caramel_client::FileRequest::new(CA_SERVER, client_id);
     let attempts = 1;
     /// Try once, does not necessarily wait for the server to sign a request
-    CR.try_loop(attempts)
+    caramel_request.try_loop(attempts)
         .expect("Something went wrong in the first attempt");
     /// Try forever. Does what it says on the tin, will retry all steps until we get a working
     /// certificate, or a hard failure that we cannot deal with.
-    CR.try_forever()
+    caramel_request.try_forever()
         .expect("Waiting forever for the server went wrong");
 }
